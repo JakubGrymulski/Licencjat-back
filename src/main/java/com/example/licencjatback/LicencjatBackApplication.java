e16758a790af3b7f8061f9ac7695216947f2e0aa
Jakub Grymulski <grymulskik7@gmail.com>
2023-02-25 15:15:02 +0100

    init

---
+++ src/main/java/com/example/licencjatback/LicencjatBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.licencjatback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicencjatBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicencjatBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/licencjatback/LicencjatBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.licencjatback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicencjatBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
